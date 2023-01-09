5f0f9400efb7d3c28a774b7dbb7882ed0a429e02
YuriSamp <yurisamp123@gmail.com>
2023-01-09 18:14:37 -0300

    adding logo and fixing some css

--- app/src/Components/NavBar/index.tsx
+++ app/src/Components/NavBar/index.tsx
@@ -1,12 +1,14 @@
 import styles from './style.module.scss';
 import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
-export const NavBar = () =>{
+export const NavBar = () => {
   const navigate = useNavigate();
-  return(
+  return (
     <div>
       <nav className={styles.navbar}>
         <div className={styles.navbar__pages}>
+          <img src={logo} alt='logo da churrascaria' className={styles.navbar__pages__logo} />
           <button className={styles.navbar__pages__buttons} onClick={() => navigate('')}>Home</button>
           <button className={styles.navbar__pages__buttons} onClick={() => navigate('/cardapio')}>Cardapio</button>
           <button className={styles.navbar__pages__buttons} onClick={() => navigate('/contato')}>Contato</button>
